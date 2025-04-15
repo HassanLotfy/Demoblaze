83f6eee05451da2154510cb876f694a4bc44923d
Mohamed Fawzy Mahmoud Omar <135051155+mohfawzy22@users.noreply.github.com>
2025-04-15 21:35:33 +0200

    script.groovy

--- script.groovy
+++ script.groovy
@@ -1,20 +1,20 @@
 def buildApp() {
     echo "🔧 Building the automation project with Maven..."
-    dir('DEPI.demoblaze') {  // Adjust this to match the actual folder name
+    dir('Demoplaza Final Project') {  // Adjust this to match the actual folder name
         bat 'mvn clean compile'
     }
 }
 
 def testApp() {
     echo "🧪 Running TestNG tests..."
-    dir('DEPI.demoblaze') {
+    dir('Demoplaza Final Project') {
         bat 'mvn test'
     }
 }
 
 def deployApp() {
     echo "📦 Archiving reports and logs..."
-    dir('DEPI.demoblaze') {
+    dir('Demoplaza Final Project') {
         junit '**/target/surefire-reports/*.xml'
         archiveArtifacts allowEmptyArchive: true, artifacts: '**/target/**/*.log, **/target/screenshots/**'
     }
