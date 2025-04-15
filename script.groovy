ea440a5a6b809c5c96ee31e74887ab85ead93980
Mohamed Fawzy Mahmoud Omar <135051155+mohfawzy22@users.noreply.github.com>
2025-04-15 23:05:12 +0200

    Add files via upload

--- script.groovy
+++ script.groovy
@@ -1,20 +1,20 @@
 def buildApp() {
     echo "🔧 Building the automation project with Maven..."
-    dir('Demoplaza Final Project') {  // Adjust this to match the actual folder name
+    dir('DEPI.demoblaze') {  // Adjust this to match the actual folder name
         bat 'mvn clean compile'
     }
 }
 
 def testApp() {
     echo "🧪 Running TestNG tests..."
-    dir('Demoplaza Final Project') {
+    dir('DEPI.demoblaze') {
         bat 'mvn test'
     }
 }
 
 def deployApp() {
     echo "📦 Archiving reports and logs..."
-    dir('Demoplaza Final Project') {
+    dir('DEPI.demoblaze') {
         junit '**/target/surefire-reports/*.xml'
         archiveArtifacts allowEmptyArchive: true, artifacts: '**/target/**/*.log, **/target/screenshots/**'
     }
